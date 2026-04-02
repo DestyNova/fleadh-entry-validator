68e4cda7dc4484a721f82859fd2898cdaf7c0033
Oisín Mac Fhearaí <denpashogai@gmail.com>
2026-04-02 20:39:49 +0100

    rm bookmarklet that shouldn't be here

--- set_fleadh_datetimes.js
+++
@@ -1,3 +0,0 @@
-var lastSlot = 19;
-Array.from(document.querySelectorAll('.vDateField')).forEach(f => { if (f.value == "") { f.value = "2025-05-18" }});
-Array.from(document.querySelectorAll('.vTimeField')).forEach(f => { if (f.value == "") { lastSlot += 1; f.value = `${lastSlot>>1}:${lastSlot%2==1?"30":"00"}` }});
